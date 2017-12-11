a043bbd2d7313516ac345d2509595064c4ae2840
TIRAS JORDAN <tiraspjordan@gmail.com>
2017-12-11 02:53:03 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -13,7 +13,7 @@ var artistName = { [objects]: 'artistName' }
 
 
 
-//var playlist = new Object({artistName: songTitle})
+var playlist = new Object({artistName: songTitle})
 
 //function updatePlaylist(playlist, artistName, songTitle){
   //return Object.assign({}, objects, {[artistName]: songTitle})
