1f1be21e992996ad479a91a7f85b5c689786ab65
TIRAS JORDAN <tiraspjordan@gmail.com>
2017-12-11 02:54:22 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,19 +1,13 @@
 var objects = {playlist}
- 
-var songTitle = { [objects]: 'songTitle' }
-var artistName = { [objects]: 'artistName' }
-
-
-
-
-
-
-
-
-
-
-
+//var songTitle = { [objects]: 'songTitle' }
+//var artistName = { [objects]: 'artistName' }
 var playlist = new Object({artistName: songTitle})
+
+
+
+
+
+
 
 //function updatePlaylist(playlist, artistName, songTitle){
   //return Object.assign({}, objects, {[artistName]: songTitle})
