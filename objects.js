51abd12929b4163fe50f0621153a95cd499c590c
TIRAS JORDAN <tiraspjordan@gmail.com>
2017-12-11 02:48:42 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -16,7 +16,7 @@ var artistName = { [objects]: 'artistName' }
 var playlist = new Object({artistName: songTitle})
 
 function updatePlaylist(playlist, artistName, songTitle){
-  return Object.assign({}, obj, {[artistName]: songTitle})
+  return Object.assign({}, objects, {[artistName]: songTitle})
   
   //playlist.artistName = ()
   //playlist.songTitle = ()
